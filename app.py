89f84536d0ddf7213fb83dda958791b6175125c3
Nicolas Surleau <surleaunicolas@gmail.com>
2023-07-18 17:36:53 +0200

    handle PNG file

--- app.py
+++ app.py
@@ -6,20 +6,22 @@ from transformers import ViltProcessor, ViltForQuestionAnswering
 from langchain.prompts import PromptTemplate
 from langchain.chains import LLMChain
 from streamlit_extras.add_vertical_space import add_vertical_space
-from langchain.llms import OpenAI
+from langchain.chat_models import ChatOpenAI
+from io import BytesIO
 
 load_dotenv()
 
 processor = ViltProcessor.from_pretrained("dandelin/vilt-b32-finetuned-vqa")
 model = ViltForQuestionAnswering.from_pretrained("dandelin/vilt-b32-finetuned-vqa")
 
-llm = OpenAI(temperature=0.2)
+llm = ChatOpenAI(temperature=0.2)
 prompt = PromptTemplate(
     input_variables=["question", "elements"],
-    template="""Please generate a structured response using the following information:
+    template="""You are a helpful assistant that can answer question related to an image. You have the ability to see the image and answer questions about it. 
+    I will give you a question and element about the image and you will answer the question.
         \n\n
         #Question: {question}
-        #Response: {elements}
+        #Elements: {elements}
         \n\n
         Your structured response:""",
     )
@@ -32,6 +34,13 @@ def process_query(image, query):
     chain = LLMChain(llm=llm, prompt=prompt)
     response = chain.run(question=query, elements=model.config.id2label[idx])
     return response
+
+def convert_png_to_jpg(image):
+    rgb_image = image.convert('RGB')
+    byte_arr = BytesIO()
+    rgb_image.save(byte_arr, format='JPEG')
+    byte_arr.seek(0)
+    return Image.open(byte_arr)
 
 # Sidebar contents
 with st.sidebar:
@@ -49,15 +58,18 @@ with st.sidebar:
 
 load_dotenv()
 
-
 def main():
     st.title("Chat with your IMAGE 💬")
 
     uploaded_file = st.file_uploader('Upload your IMAGE', type=['png', 'jpeg', 'jpg'])
 
     if uploaded_file is not None:
-        
         image = Image.open(uploaded_file)
+        
+        # ViLT model only supports JPG images
+        if image.format == 'PNG':
+            image = convert_png_to_jpg(image)
+
         st.image(image, caption='Uploaded Image.', width=300)
         
         cancel_button = st.button('Cancel')
